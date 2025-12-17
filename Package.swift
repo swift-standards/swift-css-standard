85809769f6aef2445bb2bdc48e556b4c0c33983e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-17 16:08:07 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-w3c-css", from: "0.1.2"),
+        .package(url: "https://github.com/swift-standards/swift-w3c-css", from: "0.2.1"),
         .package(url: "https://github.com/swift-standards/swift-iec-61966", from: "0.1.0"),
     ],
     targets: [
